e11ab71bcb7b919d62fb758c63d31192954fb61b
Dewang <125146872+dewang002@users.noreply.github.com>
2025-04-13 18:37:10 +0000

    change in sidebar

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,6 +1,19 @@
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideBar from "./SideBar";
+
 const MobileSidebar = ()=>{
-    return <>
-    </>
+    return <Sheet>
+        <SheetTrigger>
+            <Button className="md:hidden">
+                <Menu />
+            </Button>
+        </SheetTrigger>
+        <SheetContent>
+            <SideBar />
+        </SheetContent>
+    </Sheet>
 }
 
 export default MobileSidebar;
