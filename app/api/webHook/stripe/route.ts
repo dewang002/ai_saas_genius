aa07b37e874f33dbfe18697f7fed4c346fcbd88f
dewang002 <2002thapa@gmail.com>
2025-05-22 11:25:38 +0530

    done

--- app/api/webHook/stripe/route.ts
+++ app/api/webHook/stripe/route.ts
@@ -5,11 +5,10 @@ import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
 
 
-
 export async function POST(req: Request) {
     const body = await req.text();
-    
-    const signature =req.headers.get("stripe-signature") as string;
+
+    const signature = req.headers.get("stripe-signature") as string;
 
     let event: Stripe.Event;
 
@@ -23,7 +22,7 @@ export async function POST(req: Request) {
         const errorMessage = error instanceof Error ? error.message : 'Unknown error';
         return new NextResponse(`webhook error : ${errorMessage}`, { status: 400 });
     }
-    
+
 
 
     const session = event.data.object as Stripe.Checkout.Session;
@@ -31,7 +30,7 @@ export async function POST(req: Request) {
         try {
             const subscription = await stripe.subscriptions.retrieve(
                 session.subscription as string
-            )
+            ) as Stripe.Subscription;
 
             if (!session?.metadata?.userId) {
                 return new NextResponse("user id required", { status: 400 })
@@ -43,36 +42,32 @@ export async function POST(req: Request) {
                     stripeSubscriptionId: subscription.id,
                     stripeCustomerId: subscription.customer as string,
                     stripePriceId: subscription.items.data[0].price.id,
-                    stripeCurrentPeriodEnd: new Date(
-                        subscription.current_period_end * 1000
-                    ),
+                    stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
                 },
             })
-        } catch{
+        } catch {
             return new NextResponse("Internal error during session completion", { status: 500 });
         }
     }
 
     if (event.type === "invoice.payment_succeeded") {
-        try{
+        try {
             const subscription = await stripe.subscriptions.retrieve(
                 session.subscription as string
-            )
-            
+            ) as Stripe.Subscription
+
             await prismadb.userSubscription.update({
                 where: {
-                stripeSubscriptionId: subscription.id
-            },
-            data: {
-                stripePriceId: subscription.items.data[0].price.id,
-                stripeCurrentPeriodEnd: new Date(
-                    subscription.current_period_end * 1000
-                ),
-            },
-        })
-    } catch{
-        return new NextResponse("Internal error during invoice update",{status: 500});
-    }
+                    stripeSubscriptionId: subscription.id
+                },
+                data: {
+                    stripePriceId: subscription.items.data[0].price.id,
+                    stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+                },
+            })
+        } catch {
+            return new NextResponse("Internal error during invoice update", { status: 500 });
+        }
     }
 
     return new NextResponse(null, { status: 200 })
